0cc481b1b101502b7998d729c4b80dee4b5f1d43
VIRENPANDYA <virenpandya1975@gmail.com>
2025-04-08 13:59:47 -0400

    random

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ['http://localhost:5173'], 
+  origin: ['http://localhost:5173', 'http://forkify-production.up.railway.app'], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
